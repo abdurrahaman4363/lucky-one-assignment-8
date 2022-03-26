126ba88b5c98afdd5951f4547c72643b0d230872
abdurrahaman4363 <abdur.rahaman4363@gmail.com>
2022-03-26 09:27:26 +0600

    set event handler

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -14,7 +14,8 @@ const Shop = () => {
 
     const handleAddToCart=(product)=>{
         console.log(product);
-        
+        const newCart = [...cart,product];
+        setCart(newCart);
     }
 
     return (
@@ -30,6 +31,7 @@ const Shop = () => {
                 </div>
                 <div className="cart-container">
                   <h3>Selected Bats</h3>
+                  <p>selected items:{cart.length}</p>
                   <button style={{color:'lawngreen', borderRadius: 5,margin:'2px'}}>CHOOSE ONE FOR ME</button><br />
                   <button style={{color:'fuchsia',borderRadius: 5,margin:'2px'}}>REMOVE</button>
                 </div>
