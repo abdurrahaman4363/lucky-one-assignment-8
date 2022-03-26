e1eb13a03c12571b619f44426fbbc1dae69e0016
abdurrahaman4363 <abdur.rahaman4363@gmail.com>
2022-03-26 12:37:21 +0600

    data show in UI

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import Product from '../Product/Product';
+import './Cart.css'
 
 const Cart = ({cart}) => {
+// console.log(cart);
+/* let name;
+for(const product of cart){
+    name =product.name;
+} */
+
+const productName = cart.map(product =>product.name)
+// console.log(productName);
     return (
-        <div>
+        <div className='cart'>
                 <h3>Selected Bats</h3>
-                  <p>selected items:{cart.length}</p>
-                  <button style={{color:'lawngreen', borderRadius: 5,margin:'2px'}}>CHOOSE ONE FOR ME</button><br />
-                  <button style={{color:'fuchsia',borderRadius: 5,margin:'2px'}}>REMOVE</button>
+                  
+                 {
+                     productName.map(name => <h4>{name}</h4>)
+                 }
+                 
+                  <button style={{color:'lawngreen', borderRadius: 5,margin:'5px'}}>CHOOSE ONE FOR ME</button><br />
+                  <button style={{color:'fuchsia',borderRadius: 5,margin:'5px'}}>REMOVE</button>
         </div>
     );
 };
